5ccbf8dbfd6301dfe4b8158736c4ccb0e55e07c3
Nishu Ranjit <nishuranjit55@gmail.com>
2026-01-04 01:18:01 +0545

    Add project files.

---
+++ Controllers/StudentController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Student_Management_System.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult create()
+        {
+            return View();
+        }
+    }
+}
