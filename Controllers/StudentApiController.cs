6c73c3818969444d8b5871d5afe8e5e46c9fdb32
Nishu Ranjit <nishuranjit55@gmail.com>
2026-02-23 23:33:51 +0545

    studentapicontroller added

---
+++ Controllers/StudentApiController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SMS.Application.Interfaces;
+using SMS.Domain.Entities;
+
+namespace Student_Management_System.Controllers
+{
+    [ApiController]
+    [Route("api/Students")]
+    public class StudentApiController : Controller
+    {
+        private readonly IGenericRepository<Student> _repo;
+        public StudentApiController(IGenericRepository<Student> repo)
+
+        {
+           _repo = repo;
+        }
+        [HttpGet]
+        public async Task<IActionResult>Get()
+        {
+            return Ok(await _repo.GetAllAsync());
+        }
+    }
+}
